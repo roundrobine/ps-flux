e422ecb8539c218965e87c40789e157b1c5d17c0
Dimitar <roundrobine@gmail.com>
2020-10-15 12:39:00 +0200

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import HomePage from "./componets/HomePage";
 import React from "react";
 import { render } from "react-dom";
 import App from "./componets/App";
