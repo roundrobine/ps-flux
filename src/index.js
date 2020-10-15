459be3d4be4e965e13c2822950d9a29466fc5fdb
Dimitar <roundrobine@gmail.com>
2020-10-15 11:27:19 +0200

    create simple page server side navigation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./componets/HomePage";
+import React from "react";
+import { render } from "react-dom";
+import App from "./componets/App";
+
+render(<App />, document.getElementById("root"));
