6475f10b2a7307158a716edb4bcf800e8812c62b
Dimitar <roundrobine@gmail.com>
2020-10-20 10:55:38 +0200

    add a form for adding new course and add event onChange handler for title

--- src/componets/ManageCoursePage.jsx
+++ src/componets/ManageCoursePage.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = (props) => {
+    const [course, setCourse] = useState({
+        id: null,
+        slug: "",
+        title: "",
+        authorId: null,
+        category: "",
+    });
+
+    const handleTitleChange = (event) => {
+        const updatedCourse = { ...course, title: event.target.value };
+        setCourse(updatedCourse);
+    };
+
     return (
         <>
             <h2>Manage Courses</h2>
-            {props.match.params.slug}
+            <CourseForm course={course} onTitleChange={handleTitleChange}></CourseForm>
         </>
     );
 };
