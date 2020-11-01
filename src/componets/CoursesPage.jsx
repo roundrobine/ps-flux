661a1ba54028083c345bf07f9b369b3d2e52d09f
Dimitar Goshevski <roundrobine@gmail.com>
2020-11-01 15:59:54 +0100

    load courses on initialisation of courses page

--- src/componets/CoursesPage.jsx
+++ src/componets/CoursesPage.jsx
@@ -2,23 +2,32 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import CourseList from "./CourseList";
 import courseStore from "../stores/courseStore";
+import { loadCourses } from "../actions/courseActions";
 
 function CoursesPage() {
-    const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState(courseStore.getCourses());
 
-    useEffect(() => {
-        setCourses(courseStore.getCourses());
-    }, []);
+  useEffect(() => {
+    courseStore.addChangeListener(onChange);
+    if (courseStore.getCourses().length === 0) {
+      loadCourses();
+    }
+    return () => courseStore.removeChangeListener(onChange); //cleanup on unmount
+  }, []);
 
-    return (
-        <>
-            <h2>Courses</h2>
-            <Link className="btn btn-primary" to="/course">
-                Add Course
-            </Link>
-            <CourseList courses={courses}></CourseList>
-        </>
-    );
+  const onChange = () => {
+    setCourses(courseStore.getCourses());
+  };
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <Link className="btn btn-primary" to="/course">
+        Add Course
+      </Link>
+      <CourseList courses={courses}></CourseList>
+    </>
+  );
 }
 
 export default CoursesPage;
--- src/componets/ManageCoursePage.jsx
+++ src/componets/ManageCoursePage.jsx
@@ -5,54 +5,54 @@ import courseStore from "../stores/courseStore";
 import * as courseActions from "../actions/courseActions";
 
 const ManageCoursePage = (props) => {
-    const [errors, setErrors] = useState({});
-    const [course, setCourse] = useState({
-        id: null,
-        slug: "",
-        title: "",
-        authorId: null,
-        category: "",
+  const [errors, setErrors] = useState({});
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      setCourse(courseStore.getCourseBySlug(slug));
+    }
+  }, [props.match.params.slug]);
+
+  const handleChange = ({ target }) => {
+    setCourse({ ...course, [target.name]: target.value }); // from the path '/courses/:slug'
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseActions.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved!!!");
     });
+  };
 
-    useEffect(() => {
-        const slug = props.match.params.slug;
-        if (slug) {
-            setCourse(courseStore.getCourseBySlug(slug));
-        }
-    }, [props.match.params.slug]);
+  const formIsValid = () => {
+    const _errros = {};
 
-    const handleChange = ({ target }) => {
-        setCourse({ ...course, [target.name]: target.value }); // from the path '/courses/:slug'
-    };
+    if (!course.title) _errros.title = "Tilte is required";
+    if (!course.authorId) _errros.authorId = "Author ID is required";
+    if (!course.category) _errros.category = "Category is required";
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (!formIsValid()) return;
-        courseActions.saveCourse(course).then(() => {
-            props.history.push("/courses");
-            toast.success("Course saved!!!");
-        });
-    };
+    setErrors(_errros);
 
-    const formIsValid = () => {
-        const _errros = {};
+    //Form is valid if error object has no properties
+    return Object.keys(_errros).length === 0;
+  };
 
-        if (!course.title) _errros.title = "Tilte is required";
-        if (!course.authorId) _errros.authorId = "Author ID is required";
-        if (!course.category) _errros.category = "Category is required";
-
-        setErrors(_errros);
-
-        //Form is valid if error object has no properties
-        return Object.keys(_errros).length === 0;
-    };
-
-    return (
-        <>
-            <h2>Manage Courses</h2>
-            <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit} errors={errors}></CourseForm>
-        </>
-    );
+  return (
+    <>
+      <h2>Manage Courses</h2>
+      <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit} errors={errors}></CourseForm>
+    </>
+  );
 };
 
 export default ManageCoursePage;
