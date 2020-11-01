ccdbd3aa639e2530eba0eb450f0b2252739c9fb1
Dimitar Goshevski <roundrobine@gmail.com>
2020-11-01 17:18:36 +0100

    create update course via flux and add course listeners

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,7 +5,10 @@ import actionTypes from "./actionTypes";
 export function saveCourse(course) {
   return courseApi.saveCourse(course).then((savedCourse) => {
     // Hey dispatcher, go tell all the stores that a course was just created
-    dispatcher.dispatch({ actionType: actionTypes.CREATE_COURSE, course: savedCourse });
+    dispatcher.dispatch({
+      actionType: course.id ? actionTypes.UPDATE_COURSE : actionTypes.CREATE_COURSE,
+      course: savedCourse,
+    });
   });
 }
 
